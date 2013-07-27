c1c876244371f29cb11baf859bc7d731a9040a82
Vanderson Martins do Rosario <vandersonmr2@gmail.com>
2013-07-26 22:15:54 -0300

    VM working! Aleluia :)

--- include/paging.h
+++ include/paging.h
@@ -1,8 +1,8 @@
 #ifndef _PAGING_H_
 #define _PAGING_H_
 
-unsigned int page_directory[1024];
-unsigned int page_table[1024];
+unsigned long page_directory[1024] __attribute__ ((aligned (4096)));;
+unsigned long page_table[1024] __attribute__ ((aligned (4096)));;
 
 void enable_paging(unsigned int);
 void paging_init(void);
